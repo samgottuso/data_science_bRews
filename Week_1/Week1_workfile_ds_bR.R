9b820ad0b31f708b3b005d81baabe6ae59eb1254
Gottuso <583185@bah.com>
2018-04-12 10:14:59 -0400

    Finishing up Spread/Gather examples

--- Week_1/Week1_workfile_ds_bR.R
+++ Week_1/Week1_workfile_ds_bR.R
@@ -68,9 +68,13 @@ sw_height+sw_theme
 #Step 4-- finalize
 
 
+
+
+
+
 #start of Week 1 Material
 
-#What if we want to graph multiple ting at the same time? 
+#What if we want to make some comparative graphs that we can look at all at the same time? 
 #Use the fact_wrap formula and facet_grid
 
 #Let's say we are a slightly xenophobic physician on Corrusant and we have a diverse population of patients coming into the office. Can we look at BMI by human or non-human species?
@@ -86,46 +90,84 @@ sw_data[86,'species']='Human'
 
 #remember we use mutate to add columns to our data
 sw_data<-sw_data%>%
+  #this is just saying that if they are humans or droids keep their race as human or droid, otherwise assign them to be an alien
   mutate(race=ifelse(species=='Human','Human',ifelse(species=='Droid','Droid','Alien')))%>%
   #add blanks for those we don't need to seperately label
   mutate(tag=ifelse(race=='Alien',species,''))
 
 
-
+#load in our data, plot mass and height to x and y axis. Colour the points by gender. Add labels and adjust their position
 ggplot(data=sw_data,aes(x=mass,y=height,colour=gender))+geom_point()+geom_text(aes(label=tag),hjust=-.25,vjust=.4)+
   sw_theme+
+  #adding the grids based on race--- the scales just makes it a little easier to plot.
   facet_grid(~race,scales="free_x")
 
 
 #Next step--- how to "melt" a dataframe for graphing
 
-#often times you'll have to spread your data out to better graph it, for example if we want to graph the same 
+#often times you'll have to gather observations or spread them out to better visualize i.
+
+#Gather is typically used when we have variables that are 
 
 #located in the tidyr library (don't need this line if you just used library(tidyverse))
-
 
 library(tidyr)
 
 #reading in a different dataset, let's say that you're a marketer who is trying to advertise Jedi Personal Training Classes
-new_sw_data_gather<-read.csv('Week_1/gather_SW_data.csv')
+new_sw_data_gather<-read.csv('gather_SW_data.csv')
 
-#so let's take a look at it, it doesn't make that much sense
-View(new_sw_data_gather)
+#so let's take a look at it-- it isn't in a very nice format.
 
 #We would want to before vs after force powers, but we can't do that the way it's set up
+
+View(new_sw_data_gather)
+
+#Try to graph it, it's not right at all-- it's just showing a cordinates system instead of the progress
+
+ggplot(new_sw_data_gather,aes(x=Before.Training,y=After.Training))+geom_point()
+
+#so we can gather Before.Training and After.Training into 1 variable of 'time'. The value of each of those observations is put into the force_powers column
 
 new_sw_data_gather<-new_sw_data_gather%>%
   gather(key='Time',value='force_powers','Before.Training','After.Training')
 
-#Gather plot
+View(new_sw_data_gather)
+#That's better
+
+#make Time a factor so we can order it.
+
+new_sw_data_gather$Time<-factor(new_sw_data_gather$Time,levels=c('Before.Training','After.Training'))
+
+
+#Gather plot -- let's look at the progress our clients made from before and after training
+
+ggplot(new_sw_data_gather,aes(x=Time,y=force_powers,group=Name))+geom_point()+geom_line(colour='yellow')+geom_text(aes(label=new_sw_data_gather$Name,colour= factor(new_sw_data_gather$Side)))+
+  scale_color_manual(values=c('Blue','Red'))+
+  labs(title="Increase in Force Powers",y='Number of Force Powers')+sw_theme
 
 #Spread example
 
-new_sw_data_spread<-read.csv('Week_1/spread_SW_data.csv')
+#Let's load in the data -- it's called spread_SW_data.csv
+
+new_sw_data_spread<-
 
 View(new_sw_data_spread)
 
-new_sw_data_spread<-new_sw_data_spread%>%
-  spread(key='Type.Force.Powers',value='Number.Force.Powers')
+#What's wrong with this data if we want to look at light vs dark force powers?
 
-#Spread Plot
+#Because they are in the same column, this data isn't in the format that we want it 
+
+ggplot(new_sw_data_spread,aes(x=Type,y=Number.Force.Powers))+geom_point()
+
+
+#Let's work together to spread the data. We want to spread out the Type column into 2 new columns of light and dark.We want the values to be the number of powers
+
+new_sw_data_spread<-new_sw_data_spread%>%
+  spread()
+
+#Spread Plot-- let's look at a bar graph that shows us each Jedi's force powers
+
+ggplot(aes())+geom_bar(aes())+
+  scale_fill_manual(values=c('Red','Blue'))+
+  labs(title='Type of Force Powers by Jedi',x='Jedi',y='Number of Force Powers')+sw_theme
+
