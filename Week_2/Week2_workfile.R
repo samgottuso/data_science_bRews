c956139254f087d2a805877de2fc22584bdf3f92
Sam Gottuso <samgottuso@gmail.com>
2018-04-23 21:33:57 -0400

    Starting the cleaning process

--- Week_2/Week2_workfile.R
+++ Week_2/Week2_workfile.R
@@ -4,7 +4,10 @@
 
 #First let's look at the data that we have on Brew Reviews
 
-setwd('C:/Users/583185/Desktop/data_science/data_science_bRews/Week_2')
+#Should only have to run this once, or just do it from the file explorer
+
+unzip('beer_reviews.zip')
+
 
 beer_reviews<-read.csv('beer_reviews.csv')
 
@@ -15,3 +18,36 @@ beer_reviews<-read.csv('beer_reviews.csv')
 library(dplyr)
 
 beer_reviews<-as_tibble(beer_reviews)
+
+IPA_reviews<-filter(beer_reviews,beer_style=="American IPA")
+
+#This is better, but we still have a lot of reviews for the same beers. That could be cool to work with, but maybe we can just look at aggregates for now
+
+#Remember, the %>% allows us to do multiple 'tidy' commands at the same time
+
+IPA_reviews_agg<- IPA_reviews %>%
+  #Allows us to do operations on different groups, so in this case for each beer
+  group_by(beer_beerid)%>%
+  mutate(avg_review=mean(review_overall))%>%
+  mutate(num_review=length(review_overall)) %>%
+  ungroup() 
+  #so now we've added our average and number of reviews for each beer, but we still have every single beer review
+
+IPA_reviews_agg<-IPA_reviews_agg[unique(IPA_reviews_agg$beer_beerid),]
+
+
+##^^This isn't working, might need to ensure all of the beer id values are consistent
+
+
+#Woohoo, now we have just 1 row for each of the unique beers in our data
+
+#drop columns we don't care about we could create a list of all the ones we want to keep, or just those we want to drop
+
+IPA_reviews_agg<-select(IPA_reviews_agg,-(review_time:review_profilename))
+
+#and just a couple more
+
+IPA_reviews_agg<-select(IPA_reviews_agg,-(review_palate:review_taste))
+
+
+
